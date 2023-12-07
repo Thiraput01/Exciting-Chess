7342a28bfdb724342e20b696f4e7e431794988a8
Thiraput01 <Thiraputball@gmail.com>
2023-12-07 16:49:22 +0700

    update start screen

---
+++ src/Drawing/GameScreen.java
@@ -0,0 +1,9 @@
+package Drawing;
+
+import javafx.scene.layout.StackPane;
+
+import java.awt.*;
+
+public class GameScreen extends StackPane {
+
+}
--- src/Drawing/StartScreen.java
+++ src/Drawing/StartScreen.java
@@ -1,13 +1,19 @@
 package Drawing;
 
 
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.canvas.Canvas;
-
-import java.awt.*;
-
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import sharedObject.RenderableHolder;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class StartScreen extends BorderPane {
 
@@ -20,21 +26,86 @@ public class StartScreen extends BorderPane {
         Canvas canvas = new Canvas(1024, 768);
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        //set gc pos to start at middle
-        gc.translate((double) 1024 /2, (double) 768 /2);
-        setBackground(gc);
 
+        //set background image
+        gc.drawImage(RenderableHolder.wallpaper, 0, 0, 1024, 768);
         getChildren().add(canvas);
 
+        //create vBox to add texts, set padding to 30
+        VBox vBox = new VBox(30);
+        vBox.setAlignment(Pos.CENTER);
+        //set text font
+        Font titleFont = Font.font("Impact", FontWeight.MEDIUM, 50);
+        Font playFont = Font.font("Cochin", FontWeight.BOLD, 30);
+        Font exitFont = Font.font("Cochin", FontWeight.BOLD, 30);
+
+        //set text
+        Text title = new Text("EXCITING CHESS");
+        title.setFont(titleFont);
+        title.setFill(Color.WHITE);
+
+        Text playText = new Text("-> PLAY");
+        playText.setFont(playFont);
+        playText.setFill(Color.WHITE);
+        playText.setOpacity(0.7);
+
+        Text exitText = new Text("-> EXIT");
+        exitText.setFont(exitFont);
+        exitText.setFill(Color.WHITE);
+        exitText.setOpacity(0.7);
+
+        //add components to Vbox
+        vBox.getChildren().addAll(title, playText, exitText);
+        setCenter(vBox);
+
+        //set action on playText
+        playText.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                playText.setOpacity(1);
+            }
+        });
+
+        playText.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                playText.setOpacity(0.7);
+            }
+        });
+
+        playText.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                toMainScreen();
+            }
+        });
+
+        exitText.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                playText.setOpacity(1);
+            }
+        });
+
+        exitText.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                playText.setOpacity(0.7);
+            }
+        });
+
+        exitText.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                System.exit(0);
+            }
+        });
 
     }
 
-    public void setBackground(GraphicsContext gc){
-        //1)set bg image
 
-        //2)set play text
-
+    public void toMainScreen(){
+        RenderableHolder.clickButton.play();
     }
-
 }
 
