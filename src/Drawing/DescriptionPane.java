b4690a058ef25fff89dc18030332d4a9f476f101
Thiraput01 <Thiraputball@gmail.com>
2023-12-09 01:59:01 +0700

    Can enter and exit game

--- src/Drawing/DescriptionPane.java
+++ src/Drawing/DescriptionPane.java
@@ -1,18 +1,25 @@
 package Drawing;
 
 import GameLogic.GameLogic;
+import Main.Main;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import sharedObject.RenderableHolder;
 
 public class DescriptionPane extends BorderPane {
 
+    private Scene scene;
     public DescriptionPane() {
         super();
         setPrefSize(545, 768);
@@ -29,6 +36,12 @@ public class DescriptionPane extends BorderPane {
 
         Text exitText = new Text("Exit");
         exitText.setFont(menu_exitFont);
+        exitText.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                toStartScreen();
+            }
+        });
 
         // Add Text nodes to an HBox
         HBox textContainer = new HBox(100, menuText, exitText);
@@ -36,5 +49,15 @@ public class DescriptionPane extends BorderPane {
 
         // Set the HBox as the bottom of the BorderPane
         setBottom(textContainer);
+    }
+
+    public void toStartScreen(){
+        RenderableHolder.clickButton.play();
+        StartScreen startScreen = new StartScreen();
+        Group group = new Group(startScreen);
+        scene = new Scene(group);
+        RenderableHolder.getInstance().clear();
+        Main.stage.setScene(scene);
+        startScreen.requestFocus();
     }
 }
